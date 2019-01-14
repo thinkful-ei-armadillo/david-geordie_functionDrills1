ea0cb7c767e6e79af7a1f372f20bbb152a7b3e41
Geordie Connell <geordie.connell@gmail.com>
2019-01-14 15:08:51 -0500

    complete drill 6

--- drills.js
+++ drills.js
@@ -3,5 +3,9 @@ function createGreeting(name, age) {
     return "Hi, my name is " + name + " and I am " + age + " years old. I was born in " + yearOfBirth + ".";
 }
 
+function getYearOfBirth(age) {
+    return 2019 - age;
+}
+
 const greeting1 = createGreeting();
 console.log(greeting1);
