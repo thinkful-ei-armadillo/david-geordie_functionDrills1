c6ce01f3dfd4189c760bfede832afb5ca1622994
Geordie Connell <geordie.connell@gmail.com>
2019-01-14 15:18:07 -0500

    attempt drill 7

--- drills.js
+++ drills.js
@@ -1,11 +1,19 @@
-function createGreeting(name, age) {
-    const yearOfBirth = 2019 - age;
-    return "Hi, my name is " + name + " and I am " + age + " years old. I was born in " + yearOfBirth + ".";
-}
-
+// eslint-disable-next-line strict
 function getYearOfBirth(age) {
-    return 2019 - age;
+  if(age < 0) {
+    throw new Error('Age cannot be negative');
+  }
+  return 2019 - age;
 }
 
-const greeting1 = createGreeting();
+function createGreeting(name, age) {
+  const yearOfBirth = getYearOfBirth(age);
+  return 'Hi, my name is ' + name + ' and I am ' + age + ' years old. I was born in ' + yearOfBirth + '.';
+}
+
+try{
+    const greeting1 = createGreeting(Geordie, 28);
+} catch
+
+
 console.log(greeting1);
